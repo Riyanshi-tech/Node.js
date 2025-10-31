6cdb1bc607910704ba723b6d0d86837b59a52403
Riyanshi1 <riyanshichaudhary449@gmail.com>
2025-10-31 18:53:08 +0530

    added mongodb npm module

--- Node js/database.js
+++ Node js/database.js
@@ -1,1 +1,7 @@
- const mongoURI = "mongodb+srv://riyanshichaudhary449_db_user:dLgrTOjmnbVd0QWx@cluster0.4mh94l1.mongodb.net/" 
+  const {MongoClient}=require("mongodb")
+ const mongoURL = "mongodb+srv://riyanshichaudhary449_db_user:dLgrTOjmnbVd0QWx@cluster0.4mh94l1.mongodb.net/" 
+    const client = new MongoClient(mongoURL);
+    const dbName = "HelloWorld";
+   
+
+
