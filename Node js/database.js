6eb481fcf10cf95eaeda687d4dc53e32e461459d
Riyanshi1 <riyanshichaudhary449@gmail.com>
2025-11-01 16:50:20 +0530

    database changes

--- Node js/database.js
+++ Node js/database.js
@@ -2,6 +2,26 @@
  const mongoURL = "mongodb+srv://riyanshichaudhary449_db_user:dLgrTOjmnbVd0QWx@cluster0.4mh94l1.mongodb.net/" 
     const client = new MongoClient(mongoURL);
     const dbName = "HelloWorld";
+   async function main(){
+    await client.connect();
+
+    console.log("Connected successfully to server");
+    const db = client.db(dbName);
+    
+    const collection = db.collection("User");
+    const data = {name:"Riyanshi", age:21, city:"New Delhi"};
+
+    const insertresult = await collection.insertOne(data);
+    console.log("Inserted document =>", insertresult);  
+
+    const findresult = await collection.find({}).toArray();
+    console.log("Found documents =>", findresult);
+    return 'done';
+   }
+   main()
+     .then(console.log)
+     .catch(console.error)
+     .finally(() => client.close());
    
 
 
